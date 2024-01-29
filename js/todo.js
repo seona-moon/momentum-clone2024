45291f9b59738dba96f1d24c4f3c50e2a61b7066
seona-moon <fhflwhwl5@gmail.com>
2024-01-29 20:43:37 +0900

    Momentum clone #final
    Complete html, js!
    Let's make css~~

---
+++ js/todo.js
@@ -0,0 +1,54 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement; //클릭한 버튼이 어떤 리스트인지에 대한 정보
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //id를 이용하여 toDo 제거
+  saveToDos(); //로컬에도 변경사항 저장
+}
+
+function paintToDo(newToDoObj) {
+  const li = document.createElement("li");
+  li.id = newToDoObj.id;
+  const span = document.createElement("span");
+  span.innerText = newToDoObj.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+
+  const newToDoObj = {
+    text: toDoInput.value,
+    id: Date.now(),
+  };
+  toDoInput.value = "";
+
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  //로컬 스토리지에 값이 있으면
+  toDos = JSON.parse(savedToDos);
+  toDos.forEach(paintToDo);
+}
---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "692c3f19f59409688a6f7b717addcbb5";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you... No wheather for you...");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
