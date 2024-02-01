617fbfab2d4e60b79ce9224f49cb7b2bb24016d0
seona-moon <fhflwhwl5@gmail.com>
2024-02-01 20:07:34 +0900

    Momentum css
    more...

--- js/clock.js
+++ js/clock.js
@@ -4,9 +4,9 @@ function getClock() {
   const date = new Date();
   const hour = date.getHours().toString().padStart(2, "0");
   const min = String(date.getMinutes()).padStart(2, "0");
-  const sec = String(date.getSeconds()).padStart(2, "0");
+  //const sec = String(date.getSeconds()).padStart(2, "0");
 
-  clock.innerText = `${hour}:${min}:${sec}`;
+  clock.innerText = `${hour}:${min}`;
   //   clock.innerText = new Date().toLocaleTimeString("es-US", { hour12: false });
 }
 
